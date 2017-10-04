c303d5f58ce1e0ab30e76e6c5ca7c1622619c3a5
ms383 <ms383@pc3-045-l.cs.st-andrews.ac.uk>
2017-10-04 23:07:25 +0100

    finish the project and pass the auto cheaker

--- src/AlignText.java
+++ src/AlignText.java
@@ -1,43 +1,73 @@
-import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.regex.Pattern;
 
 public class AlignText {
-	private char just;
-	public static char LEFT = 'L';
-	public static char RIGHT = 'R';
-	public static char CENTER = 'C';
-	public static char JUSTIFY = 'J';
-	
-	public void show(ArrayList arraylist) {
+	public void show(ArrayList<String> arraylist) {
 		for(int i = 0; i < arraylist.size(); i++)  
 			System.out.println(arraylist.get(i));
 	}  
 	
+	public static boolean isInteger(String str){
+//		Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
+//		return pattern.matcher(str).matches();
+		for(int i = 0; i < str.length(); i++){
+			int chr = str.charAt(i);
+			if (chr < 48 || chr > 57) return false;
+		}
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		AlignText aligntext = new AlignText();
 		try {
-			if ((args.length < 2) )
-				throw new IllegalInputException("java AlignText file_name line_length")
-		}
-		if (args.length < 2) 
-			System.out.println("java AlignText file_name line_length");
-		else {
-			String[] paragraphs = FileUtil.readFile(args[0]);			
-			int length = new Integer(args[1]);
-			for(int i = 0; i < paragraphs.length; i++) 
-				System.out.println(paragraphs[i]);
-			aligntext.show(SplitFile.split(paragraphs, length));
+			//get values
+			String[] paragraphs = FileUtil.readFile(args[0]);
+			if (aligntext.isInteger(args[1])){
+				//change the form of input from string to integer
+				int length = new Integer(args[1]);
+				
+				for(int i = 0; i < paragraphs.length; i++){
+					ArrayList<String> result = SplitFile.split(paragraphs[i], length);
+					if (args.length == 3) {
+						switch(args[2]) {
+							case "L": 
+								aligntext.show(SplitFile.left(result, length));
+								break;
+							case "C": 
+								aligntext.show(SplitFile.centre(result, length));
+								break;
+							case "J": 
+								aligntext.show(SplitFile.justify(result, length));
+								break;
+							default:System.out.println("");
+						}
+					}else aligntext.show(SplitFile.right(result, length));
+				}
+			}else System.out.println("usage: java AlignText file_name line_length <align_mode>");
 			
+			
+//			//split 
+//			ArrayList<String> result = SplitFile.split(paragraphs, length);
+//			//how to show				
 //			if (args.length == 3) {
 //				switch(args[2]) {
-//				case "L": ;
-//				case "C": ;
-//				case "J": ;
-//				default:System.out.println("");
+//					case "L": 
+//						aligntext.show(SplitFile.left(result, length));
+//						break;
+//					case "C": 
+//						aligntext.show(SplitFile.centre(result, length));
+//						break;
+//					case "J": 
+//						aligntext.show(SplitFile.justify(result, length));
+//						break;
+//					default:System.out.println("");
 //				}
-//			}
-		}
+//			}else aligntext.show(SplitFile.right(result, length));
+			
+			
+		}catch(Exception e){
+			System.out.println("usage: java AlignText file_name line_length <align_mode>");
+		}		
 	}
 }
 
--- src/SplitFile.java
+++ src/SplitFile.java
@@ -2,9 +2,26 @@ import java.util.ArrayList;
 
 public class SplitFile {
 	
-	public static ArrayList<String> split(String[] paragraphs, int length) {
+	public static ArrayList<String> split(String paragraph, int length) {
 		ArrayList<String> line = new ArrayList<>();
-		for(int i = 0; i < paragraphs.length; i++) {	
+		String str = "";
+		String[] words = paragraph.split(" ");
+		for(int n = 0; n < words.length; n++){
+			if (str == ""){                                                                                                       
+				str = words[n];
+			}else if ((str + " " + words[n]).length() <= length){
+				str = str + " " + words[n];	
+			}else if (words[n].length() >= length){
+				line.add(str);
+				str = words[n];
+			}else{
+				line.add(str);
+				str = words[n];
+			}
+		}
+		line.add(str);
+		return line;
+		/*for(int i = 0; i < paragraphs.length; i++) {	
 			int p = 0;
 			while((paragraphs[i].length() - p) > length) {
 				if(paragraphs[i].substring(p + length - 1, p + length).equals(" ")) {
@@ -27,8 +44,116 @@ public class SplitFile {
 					}
 				}
 			} 
+		}*/
+		
+		
+//		for(int i = 0; i < paragraphs.length; i++) {	
+//			String str = "";
+//			String[] words = paragraphs[i].split(" ");
+//			for(int n = 0; n < words.length; n++){
+//				if (str == ""){                                                                                                       
+//					str = words[n];
+//				}else if ((str + " " + words[n]).length() <= length){
+//					str = str + " " + words[n];	
+//				}else if (words[n].length() >= length){
+//					line.add(str);
+//					str = words[n];
+//				}else{
+//					line.add(str);
+//					str = words[n];
+//				}
+//			}
+//			line.add(str);
+//		}
+//		return line;
+		
+		
+	}
+	
+	protected final static String genespace(int n){//static?????
+		String to = "";
+		for(int i = 0; i < n; i++){
+			to = to + " ";
 		}
-		return line;
+		return to;
+	}
+	
+	public static ArrayList<String> right(ArrayList<String> line, int length){
+		String str = "";
+		ArrayList<String> result = new ArrayList<String>();
+		for(int i = 0; i < line.size(); i++){
+			str = line.get(i);
+			if (length - str.length() > 0){
+				str = genespace(length-str.length()) + str;
+				result.add(str);
+			}else result.add(str);
+			
+		}
+		return result;
+	}
+	
+	public static ArrayList<String> left(ArrayList<String> line, int length){
+		String str = "";
+		ArrayList<String> result = new ArrayList<String>();
+		for(int i = 0; i < line.size(); i++){
+			str = line.get(i);
+			if (length - str.length() > 0){
+				str = str + genespace(length-str.length());
+				result.add(str);
+			}else result.add(str);
+		}
+		return result;
+	}
+
+	public static ArrayList<String> centre(ArrayList<String> line, int length){
+		String str = "";
+		ArrayList<String> result = new ArrayList<String>();
+		for(int i = 0; i < line.size(); i++){
+			str = line.get(i);
+			if (length - str.length() > 0){
+				if ((length - str.length()) % 2 == 0){
+					str = genespace((length - str.length())/2) + str + genespace((length - str.length())/2);
+					result.add(str);
+				}else{
+					str = genespace((length - str.length())/2 + 1) + str + genespace((length - str.length())/2);
+					result.add(str);
+				}
+			}else result.add(str);
+		}
+		return result;
+	}
+
+	public static ArrayList<String> justify(ArrayList<String> line, int length){
+		ArrayList<String> result = new ArrayList<String>();
+		String[] words;
+		int nGap = 0;
+		int i = 0;
+		for(i = 0; i < line.size() - 1; i++){
+			String str = "";
+			words = line.get(i).split(" ");
+			//get the number of spaces need to be added
+			int nChar = 0;
+			for(int j = 0; j < words.length; j++) nChar += words[j].length();
+			int nSpace = length - nChar;
+			//number of gap
+			nGap = (words.length) - 1;
+			if (nGap == 0){
+				str = words[0];
+				result.add(str);
+			}else{
+				int neGap = nSpace % nGap; 
+				int n;
+				for(n = 0; n < nGap; n++){
+					if (n < nGap- neGap){
+						str = str + words[n] + genespace(nSpace / nGap);
+					}else str = str + words[n] + genespace(nSpace / nGap + 1);
+				}
+				str = str + words[n];
+				result.add(str);
+			}
+		}
+		result.add(line.get(i));
+		return result;
 	}
 
 }
